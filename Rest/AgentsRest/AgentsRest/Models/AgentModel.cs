921bb9bac80770421363aed99de92af8df98f15b
shmuel <shmuel.hor@gmail.com>
2024-08-21 14:23:24 +0300

    create init

---
+++ Rest/AgentsRest/AgentsRest/Models/AgentModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AgentsRest.Models
+{
+    public enum Status
+    {
+        IsActive = 1,
+        IsNotActive = 0
+    }
+    public class AgentModel
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string NickName { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public Status Status { get; set; }
+    }
+}
